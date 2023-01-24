8f47104caad2ffa63e703db3b1b48be4549eabde
Nima Prmi <nimaprmdi@gmail.com>
2023-01-24 11:59:32 +0330

    Added new city

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -4,11 +4,13 @@ import BoundingBox from "./BoundingBox";
 import Dragable from "./Dragable";
 
 const Car = () => {
-    return (
-        <Suspense fallback={null}>
-            <Model path="/models/cities/semnan2.glb" scale={new Array(3).fill(10)} />
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={null}>
+      {/* <Model path="/models/cities/semnan2.glb" scale={new Array(3).fill(10)} /> */}
+
+      <Model path="/models/cities/zanjan.glb" scale={[10, 10, 10]} position={[1, 1, 1]} />
+    </Suspense>
+  );
 };
 
 export default Car;
