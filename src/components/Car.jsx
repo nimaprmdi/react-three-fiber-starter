217222c337f6f131bb6a5a8c1782080cb932c965
Nima Prmdi <nimaprmdi@gmail.com>
2023-01-24 07:10:11 +0330

    Added City 3D Model

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -6,17 +6,7 @@ import Dragable from "./Dragable";
 const Car = () => {
     return (
         <Suspense fallback={null}>
-            <Dragable transformGroup>
-                <BoundingBox visible position={[4, 3, 0]} dims={[3, 2, 6]} offset={[0, -1, 0]}>
-                    <Model path="/models/cyber_truck/scene.gltf" scale={new Array(3).fill(1)} />
-                </BoundingBox>
-            </Dragable>
-
-            <Dragable transformGroup>
-                <BoundingBox visible position={[-4, 3, 0]} dims={[3, 2, 6]} offset={[0, -0.8, 0]}>
-                    <Model path="/models/model_s/scene.gltf" scale={new Array(3).fill(0.01)} />
-                </BoundingBox>
-            </Dragable>
+            <Model path="/models/cities/semnan2.glb" scale={new Array(3).fill(10)} />
         </Suspense>
     );
 };
